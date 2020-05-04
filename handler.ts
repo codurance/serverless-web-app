26dad8b633d7de4e389742214c6fffca6e3d28a5
Jocelyn FACCHINI <jocelyn.facchini@gmail.com>
2020-05-04 12:50:28 +0100

    Add .circleci/config.yml

--- handler.ts
+++ handler.ts
@@ -1,12 +1,15 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import 'source-map-support/register';
 
-export const hello: APIGatewayProxyHandler = async (event, _context) => {
+export const hello: APIGatewayProxyHandler = async (_event, _context) => {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
-      input: event,
-    }, null, 2),
+      result: "Hello world"
+    })
+    // body: JSON.stringify({
+    //   message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
+    //   input: event,
+    // }, null, 2),
   };
 }
