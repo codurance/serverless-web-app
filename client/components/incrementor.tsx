504fea747fd47381c5c511e5e2e4fa86688ff46c
Jocelyn Facchini <jocelyn.facchini@codurance.com>
2020-05-07 13:04:10 +0100

    Test increment by 1 [skip ci]

--- client/components/incrementor.tsx
+++ client/components/incrementor.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function Incrementor() {
-    return (<div></div>);
+    return (<div className="numberLabel">0</div>);
 };
--- client/tests/unit/incrementor.spec.tsx
+++ client/tests/unit/incrementor.spec.tsx
@@ -3,9 +3,20 @@ import React from "react";
 import Incrementor from "../../components/incrementor";
 
 describe("Incrementor", () => {
+    const wrapper = mount(<Incrementor />);
+    const label = wrapper.find('.numberLabel').first();
+
     it("should start with 0", () => {
-        const wrapper = mount(<Incrementor />);
-        const label = wrapper.find('.numberLabel').first();
         expect(label.text()).toEqual("0");
+    });
+
+    describe('when I click increment button', () => {
+        const incrementButton = wrapper.find('[data-test=incrementButton]').first();
+
+        it('should increment the label by 1', () => {
+            incrementButton.simulate("click");
+
+            expect(label.text()).toEqual("1");
+        });
     });
 })
