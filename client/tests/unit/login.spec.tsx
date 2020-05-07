8557fd895a8d25631ea118bf59f2954f0146a5a3
Alasdair Smith <alasdairsmith0491@gmail.com>
2020-05-07 15:26:06 +0100

    --wip-- [skip ci]

--- client/tests/unit/login.spec.tsx
+++ client/tests/unit/login.spec.tsx
@@ -5,8 +5,16 @@ import LoginForm from "../../components/loginForm";
 describe("Login form component", () => {
   const wrapper = mount(<LoginForm />);
   describe("when no field were filled", () => {
+    it("should not display an error initially", () => {
+      const errorLabel = wrapper.find("[data-test='errorLabel']").at(0);
+
+      expect(errorLabel.text()).toEqual("");
+     
+    });
+
     it("should display an error: Please input your credentials", () => {
       const errorLabel = wrapper.find("[data-test='errorLabel']").at(0);
+
       expect(errorLabel.text()).toEqual("Please input your credentials");
     });
   });
